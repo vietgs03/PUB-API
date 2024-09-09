0f5cd75ddb6f7c87da1c10e8892a3e74ad2b30da
DESKTOP-QV8266K.ngodanghuy061 <DESKTOP-QV8266K.ngodanghuy061@gmail.com>
2024-09-10 00:38:25 +0700

    Mô tả thay đổi của bạn

--- src/models/repositories/vatttulist.repo.js
+++ src/models/repositories/vatttulist.repo.js
@@ -1,45 +1,44 @@
-// repo.js
-const db = require('../../dbs/init.mysqldb');
-const getConnection = db.getConnection();
+    // repo.js
+    const db = require('../../dbs/init.mysqldb');
+    const getConnection = db.getConnection();
 
 
 
-const createIndexIfNotExists = async () => {
-    const query = `
-        CREATE INDEX IF NOT EXISTS idx_mavattu ON llx_vattu(mavattu);
-    `;
-    try {
-        await db.executeQuery(query);
-        console.log('Index created successfully or already exists.');
-    } catch (error) {
-        console.error('Failed to create index:', error);
-        throw error;
-    }
-};
+    const createIndexIfNotExists = async () => {
+        const query = `
+            CREATE INDEX IF NOT EXISTS idx_mavattu ON llx_vattu(mavattu);
+        `;
+        try {
+            await db.executeQuery(query);
+            console.log('Index created successfully or already exists.');
+        } catch (error) {
+            console.error('Failed to create index:', error);
+            throw error;
+        }
+    };
 
 
-createIndexIfNotExists();
+    createIndexIfNotExists();
 
 
-const getVattuList = async ({ limit = 12000, offset = 0 }) => {
-   
-   
-    const query = `
-      SELECT  pre.mavattu, pre.tenvattu, pre.donvi, pre.macu, pre.status
-    FROM (
-        SELECT  mavattu
-        FROM llx_vattu
-        ORDER BY mavattu ASC
-        LIMIT ? OFFSET ?
-    ) AS temp
-    INNER JOIN llx_vattu AS pre ON temp.mavattu = pre.mavattu
-    ORDER BY pre.mavattu ASC; `;
-
- 
-    return await db.executeQuery(query, [parseInt(limit), parseInt(offset)]);
-
+    const getVattuList = async ({ limit = 100, lastMavattu = null }) => {
+        let query = `
+            SELECT pre.mavattu, pre.tenvattu, pre.donvi, pre.macu, pre.status
+            FROM llx_vattu AS pre
+        `;
     
-   
-}
-
-module.exports = { getVattuList };
+        const params = [];
+    
+        if (lastMavattu) {
+            // Điều kiện để lấy các bản ghi có mavattu lớn hơn lastMavattu (con trỏ)
+            query += ` WHERE pre.mavattu > ?`;
+            params.push(lastMavattu);
+        }
+    
+        query += ` ORDER BY pre.mavattu ASC LIMIT ?`;
+        params.push(parseInt(limit));
+    
+        return await db.executeQuery(query, params);
+    };
+    
+    module.exports = { getVattuList };
